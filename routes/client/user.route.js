ed5866cc116d1a8df5fdfb1debf313f52e197bf5
Vuquanghuybm2k6 <vuquanghuybm2k6@gmail.com>
2026-02-27 11:11:53 +0700

    Tạo phần upload avatar cho tài khoản

--- routes/client/user.route.js
+++ routes/client/user.route.js
@@ -1,6 +1,13 @@
 const express = require("express")
 const router = express.Router()
 const controller = require("../../controllers/client/user.controller")
+const uploadCloud = require("../../middlewares/client/uploadCloud.middleware")
+const multer = require("multer")
+const upload = multer()
 router.get('/register', controller.register)
-router.post('/register', controller.registerPost)
+router.post(
+  '/register',
+  upload.single("avatar"),
+  uploadCloud.upload,
+  controller.registerPost)
 module.exports = router
