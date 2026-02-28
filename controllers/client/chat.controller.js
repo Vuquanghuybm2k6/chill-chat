601c3740c1f54eb5dde0429525131beaacff07e5
Vuquanghuybm2k6 <vuquanghuybm2k6@gmail.com>
2026-02-28 18:12:28 +0700

    Tính năng chat

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -1,10 +1,18 @@
 const chatSocket = require("../../sockets/client/chat.socket")
+const Chat = require("../../models/chat.model")
+const User = require("../../models/user.model")
 // [GET]: /chat
-module.exports.index = (req, res) => {
+module.exports.index = async (req, res) => {
   // Socket io
   chatSocket(req,res)
   // End Socket io
+  const chats = await Chat.find({deleted: false})
+  for(const chat of chats){
+    const infoUser = await User.findOne({_id: chat.user_id, deleted: false}).select("fullName")
+    chat.infoUser = infoUser
+  }
   res.render("client/pages/chat/index", {
-    pageTitle: "Kênh chat"
+    pageTitle: "Kênh chat",
+    chats: chats
   })
 }
--- sockets/client/chat.socket.js
+++ sockets/client/chat.socket.js
@@ -1,6 +1,13 @@
-
-module.exports = async(io) =>{
-  _io.on('connection',(socket)=>{
-    console.log('a user connected', socket.id)
+const Chat = require("../../models/chat.model")
+module.exports = async (req,res) =>{
+  const userId = res.locals.user.id
+  _io.once('connection', (socket)=>{
+    socket.on("CLIENT_SEND_MESSAGE", async (content)=>{
+      const chat = new Chat({
+        user_id: userId,
+        content: content
+      })
+      await chat.save()
+    })
   })
 }
