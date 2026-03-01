8b76eebce674bc36f69d0942978ecdda8630652b
Vuquanghuybm2k6 <vuquanghuybm2k6@gmail.com>
2026-03-01 15:16:31 +0700

    Tính năng chat realtime

--- public/client/js/chat.js
+++ public/client/js/chat.js
@@ -11,3 +11,25 @@ if(formSendData){
   })
 }
 // END SERVER_SEND_MESSAGE
+
+// SERVER_RETURN_MESSAGE
+socket.on("SERVER_RETURN_MESSAGE", (data)=>{
+  const myId = document.querySelector("[my-id]").getAttribute("my-id")
+  const body = document.querySelector(".inner-body")
+  const div = document.createElement("div")
+  let htmlFullName = ""
+  if(myId == data.userId){
+    div.classList.add("inner-outgoing")
+  }
+  else{
+    div.classList.add("inner-outgoing")
+    htmlFullName = `<div class = "inner-name">${data.fullName}</div>`
+  }
+  div.innerHTML = 
+  `
+  ${htmlFullName}
+  <div class = "inner-content">${data.content}</div>
+  `
+  body.appendChild(div)
+})
+// END SERVER_RETURN_MESSAGE
--- sockets/client/chat.socket.js
+++ sockets/client/chat.socket.js
@@ -1,13 +1,21 @@
 const Chat = require("../../models/chat.model")
 module.exports = async (req,res) =>{
   const userId = res.locals.user.id
+  const fullName = res.locals.user.fullName
   _io.once('connection', (socket)=>{
     socket.on("CLIENT_SEND_MESSAGE", async (content)=>{
+      // Lưu vào db
       const chat = new Chat({
         user_id: userId,
         content: content
       })
       await chat.save()
+      // Trả data về client
+      _io.emit("SERVER_RETURN_MESSAGE",{
+        userId: userId,
+        fullName: fullName,
+        content: content
+      })
     })
   })
 }
