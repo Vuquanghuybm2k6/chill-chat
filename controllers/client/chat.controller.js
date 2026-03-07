0228084ce4e3251fd4eab7b1f04e7d07c807b8e4
Vuquanghuybm2k6 <vuquanghuybm2k6@gmail.com>
2026-03-07 13:39:33 +0700

    Hiển thị danh sách tin nhắn đoạn chat theo phòng chat

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -1,12 +1,13 @@
 const chatSocket = require("../../sockets/client/chat.socket")
 const Chat = require("../../models/chat.model")
 const User = require("../../models/user.model")
-// [GET]: /chat
+// [GET]: /chat/:roomChatId
 module.exports.index = async (req, res) => {
+  const roomChatId = req.params.roomChatId
   // Socket io
   chatSocket(req,res)
   // End Socket io
-  const chats = await Chat.find({deleted: false})
+  const chats = await Chat.find({deleted: false, room_chat_id: roomChatId})
   for(const chat of chats){
     const infoUser = await User.findOne({_id: chat.user_id, deleted: false}).select("fullName")
     chat.infoUser = infoUser
--- sockets/client/chat.socket.js
+++ sockets/client/chat.socket.js
@@ -2,20 +2,23 @@ const Chat = require("../../models/chat.model")
 module.exports = async (req,res) =>{
   const userId = res.locals.user.id
   const fullName = res.locals.user.fullName
+  const roomChatId = req.params.roomChatId
   _io.on('connection', (socket)=>{
-
+    socket.join(roomChatId)
     socket.on("CLIENT_SEND_MESSAGE", async (content)=>{
       // Lưu vào db
       const chat = new Chat({
         user_id: userId,
-        content: content
+        content: content,
+        room_chat_id: roomChatId
       })
       await chat.save()
       // Trả data về client
-      _io.emit("SERVER_RETURN_MESSAGE",{
+      _io.to(roomChatId).emit("SERVER_RETURN_MESSAGE",{
         userId: userId,
         fullName: fullName,
-        content: content
+        content: content,
+        room_chat_id: roomChatId
       })
     })
   })
